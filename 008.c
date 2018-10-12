771511d03448e63d94b77c23ebf4fd097a7bea04
hellowujiayi <w.45@qq.com>
2018-10-12 20:44:54 +0800

    updata

--- 008.c
+++ 008.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
 int res(int number) {
-    if (number > 1) {
+    if (number > 1) 
        return number * res(number - 1); 
-    }
 }
 
 int main() {
-    int number_input = 0, number_output = 0;
+    int number_input = 0;
     scanf("%d",&number_input);
-    number_output = res(number_input);
-    printf("%d! = %d", number_input, number_output);
+    printf("%d! = %d", number_input, res(number_input));
     return 0;
 }
